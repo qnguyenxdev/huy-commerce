0332e8765b9f3382dc9532958e79051607bf4ff1
Huy Nguyen <quanghuy.nguyen@haw-hamburg.de>
2025-04-19 22:53:53 +0200

    [IDENTITY] fix invalid format in detect-changes workflow (WIP)

--- identity-service/src/main/java/com/qnguyenxdev/identity/controller/UserController.java
+++ identity-service/src/main/java/com/qnguyenxdev/identity/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.qnguyenxdev.identity.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 }
